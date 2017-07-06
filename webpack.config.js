e252cd4ff8a7b2c4dc4221c24b5249f2c5f196f8
Philipp Anders <philipp.anders.pa@gmail.com>
2017-07-07 00:49:19 +0200

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: __dirname + "/js/app.js",
+    output: {
+        path: __dirname + "/build/js",
+        publicPath: '/build/js/',
+        filename: "app.bundle.js"
+    },
+    plugins: [
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devServer: {
+        port: 8000
+    },
+    devtool: 'source-map'
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,5 @@
+var config = require('./webpack.config');
+
+config.devtool = false;
+
+module.exports = config;
